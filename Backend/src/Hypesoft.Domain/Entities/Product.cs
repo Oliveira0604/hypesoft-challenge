e2bdb893a87bc615daf37b177e12bb85fe707054
oliveira <oliveiranathan853@gmail.com>
2026-02-26 16:44:24 -0300

    refactor(product): improve to register CreatedProductEvent and LowStockEvent

--- Backend/src/Hypesoft.Domain/Entities/Product.cs
+++ Backend/src/Hypesoft.Domain/Entities/Product.cs
@@ -1,6 +1,7 @@
 namespace Hypesoft.Domain.Entities;
 
 using Hypesoft.Domain.ValueObjects;
+using Hypesoft.Domain.DomainEvents;
 public class Product
 {
     public string Id { get; private set; } = Guid.NewGuid().ToString();
@@ -10,6 +11,13 @@ public class Product
     public ProductPrice Price { get; private set; }
     public ProductCategoryId CategoryId { get; private set; }
     public ProductQuantity StockQuantity { get; private set; }
+
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    private void AddDomainEvent(IDomainEvent eventItem) => _domainEvents.Add(eventItem);
 
     public Product(
         ProductName name,
@@ -25,6 +33,8 @@ public class Product
         Price = price;
         CategoryId = categoryId;
         StockQuantity = stockQuantity;
+
+        AddDomainEvent(new ProductCreatedEvent(Id, name.Value));
     }
 
     public void UpdateName(string name)
@@ -45,5 +55,10 @@ public class Product
     public void UpdateStock(int quantity)
     {
         StockQuantity = new ProductQuantity(StockQuantity.Value + quantity);
+
+        if (StockQuantity.Value < 10)
+        {
+            AddDomainEvent(new LowStockEvent(Id, StockQuantity.Value));
+        }
     }
 }

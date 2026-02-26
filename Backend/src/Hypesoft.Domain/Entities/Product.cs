3b765f37258cface3a4d64a478feeef4e446374d
oliveira <oliveiranathan853@gmail.com>
2026-02-26 11:50:17 -0300

    feat(domain): add Product entity and validations

---
+++ Backend/src/Hypesoft.Domain/Entities/Product.cs
@@ -0,0 +1,74 @@
+namespace Hypesoft.Domain.Entities;
+
+public class Product
+{
+    public string Id { get; private set; } = Guid.NewGuid().ToString();
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public decimal Price { get; private set; }
+    public string CategoryId { get; private set; }
+    public int StockQuantity { get; private set; }
+
+    public Product(
+        string name,
+        string description,
+        decimal price,
+        string categoryId,
+        int stockQuantity
+    )
+
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Product name cannot be empty.");
+
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Product description cannot be empty.");
+
+        if (price < 0)
+            throw new ArgumentException("Price cannot be negative.");
+
+        if (stockQuantity < 0)
+            throw new ArgumentException("Stock quantity cannot be negative.");
+
+        if (string.IsNullOrWhiteSpace(categoryId))
+            throw new ArgumentException("Category ID cannot be empty.");
+
+        Name = name;
+        Description = description;
+        Price = price;
+        CategoryId = categoryId;
+        StockQuantity = stockQuantity;
+    }
+
+    public void UpdateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Product name cannot be empty.");
+
+        Name = name;
+    }
+
+    public void UpdateDescription(string description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Product description cannot be empty.");
+
+        Description = description;
+    }
+
+    public void UpdatePrice(decimal price)
+    {
+        if (price < 0)
+            throw new ArgumentException("Price cannot be negative.");
+
+        Price = price;
+    }
+
+    public void UpdateStock(int quantity)
+    {
+        if (quantity < 0)
+            throw new ArgumentException("Quantity cannot be negative.");
+        StockQuantity += quantity;
+    }
+}
